a4447fa3860346f1da8c8f3ca0dd82466b29e250
Brenden T <brenden.trieu@gmail.com>
2023-10-09 01:17:57 -0700

    set up done ish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+//import "../../node_modules/bootstrap";
+
 export default function Home() {
   return (
     <div>
+      <button type="button" className="btn btn-outline-primary">
+        Primary
+      </button>
       <p>page start</p>
     </div>
   );
