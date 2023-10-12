e735cfe60de88105b038a83788a3741ca925ba37
Brenden T <brenden.trieu@gmail.com>
2023-10-12 16:57:13 -0700

    icon works very shmall

---
+++ src/app/components/linked_icons/linkedIn.tsx
@@ -0,0 +1,17 @@
+import styles from "../../styles/linked_icons.module.css";
+
+const LinkedIn = () => {
+  return (
+    <>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css"
+      ></link>
+      <div>
+        <i className="bi bi-linkedin"></i>
+      </div>
+    </>
+  );
+};
+
+export default LinkedIn;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import LinkedIn from "./components/linked_icons/LinkedIn";
+
 const Home = () => {
   return (
     <div>
+      <LinkedIn />
       <button type="button" className="btn btn-outline-primary">
         Primary
       </button>
