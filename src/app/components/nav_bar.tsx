3691bf3ea928c33c698848b79c389ceb97400bf5
Brenden T <brenden.trieu@gmail.com>
2023-10-11 21:28:20 -0700

    Update nav_bar.tsx

--- src/app/components/nav_bar.tsx
+++ src/app/components/nav_bar.tsx
@@ -3,6 +3,20 @@ import styles from "../styles/navBar.module.css";
 
 const NavBar = () => {
   const stickyRef = useRef(null);
+
+  return (
+    <div>
+      <div>
+        <a href={"/"}>About Me</a>
+      </div>
+      <div>
+        <a href={"/"}>Projects</a>
+      </div>
+      <div>
+        <a href={"/"}>Resume</a>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
