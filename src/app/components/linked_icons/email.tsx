059ca1f33c4b7787cbf9cffb5ab3bda991f44fb2
Brenden T <brenden.trieu@gmail.com>
2023-10-19 17:46:25 -0700

    Update email.tsx

--- src/app/components/linked_icons/email.tsx
+++ src/app/components/linked_icons/email.tsx
@@ -3,7 +3,7 @@ import styles from "../../styles/linkedIcons.module.css";
 const Email = () => {
   return (
     <>
-      <div className={styles.iconBackground}>
+      <div className={`${styles.iconBackground} ${styles.email}`}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="16"
