411a40e3fb3af6f38cd0e01c4f9b5068897b37c0
Brenden T <brenden.trieu@gmail.com>
2023-11-16 17:19:09 -0800

    updates

--- src/app/segments/aboutMe.tsx
+++ src/app/segments/aboutMe.tsx
@@ -9,12 +9,20 @@ const AboutMe = () => {
       <h2>About Me</h2>
       <div className={styles.mainContent}>
         <p>
-          This is the summary of who I am and that Im a full stack developer who
-          is always eager to learn new things
+          As a relentless and ever-evolving full-stack developer, I am committed
+          to continuous learning and creating meaningful impacts wherever I go.
+          A proud graduate of the University of British Columbia with a degree
+          in computer science, my lifelong passion for software development has
+          driven me to excel in full-stack development, supported by valuable
+          professional experience gained from my Co-op internship at Atomic47. I
+          am dedicated to crafting efficient and innovative solutions for every
+          challenge I encounter, with a vision to empower those around me. If
+          you share this mindset, I invite you to explore my projects and join
+          me on this inspiring journey of limitless possibilities.
         </p>
         <img
           src="assets\imgs\1579569910515.jfif"
-          alt="img of face"
+          alt="img of me"
           className={styles.imgGallary}
         ></img>
       </div>
