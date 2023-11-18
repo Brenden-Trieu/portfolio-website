de29bbab302614e4d2fcaa6f10b73cbe09ed83ea
Brenden T <brenden.trieu@gmail.com>
2023-11-17 16:24:10 -0800

    updates

--- src/app/segments/aboutMe.tsx
+++ src/app/segments/aboutMe.tsx
@@ -6,25 +6,28 @@ import ReactIcon from "../components/icons/standard_icons/ReactIcon";
 const AboutMe = () => {
   return (
     <div id="aboutMe_section" className={styles.section}>
-      <h2>About Me</h2>
-      <div className={styles.mainContent}>
-        <p>
-          As a relentless and ever-evolving full-stack developer, I am committed
-          to continuous learning and creating meaningful impacts wherever I go.
-          A proud graduate of the University of British Columbia with a degree
-          in computer science, my lifelong passion for software development has
-          driven me to excel in full-stack development, supported by valuable
-          professional experience gained from my Co-op internship at Atomic47. I
-          am dedicated to crafting efficient and innovative solutions for every
-          challenge I encounter, with a vision to empower those around me. If
-          you share this mindset, I invite you to explore my projects and join
-          me on this inspiring journey of limitless possibilities.
-        </p>
-        <img
-          src="assets\imgs\1579569910515.jfif"
-          alt="img of me"
-          className={styles.imgGallary}
-        ></img>
+      <div className={styles.titleAndContent}>
+        <h2>About Me</h2>
+        <div className={styles.mainContent}>
+          <p>
+            As a relentless and ever-evolving full-stack developer, I am
+            committed to continuous learning and creating meaningful impacts
+            wherever I go. A proud graduate of the University of British
+            Columbia with a degree in computer science, my lifelong passion for
+            software development has driven me to excel in full-stack
+            development, supported by valuable professional experience gained
+            from my Co-op internship at Atomic47. I am dedicated to crafting
+            efficient and innovative solutions for every challenge I encounter,
+            with a vision to empower those around me. If you share this mindset,
+            I invite you to explore my projects and join me on this inspiring
+            journey of limitless possibilities.
+          </p>
+          <img
+            src="assets\imgs\1579569910515.jfif"
+            alt="img of me"
+            className={styles.imgGallary}
+          ></img>
+        </div>
       </div>
       <div className={styles.listings}>
         <p>Worked with</p>
