6b3d7345dfabdc251dbf532211ea79c7e9760d5f
Brenden T <brenden.trieu@gmail.com>
2023-11-28 14:52:37 -0800

    done just need responsive for mobile

--- src/app/segments/footer.tsx
+++ src/app/segments/footer.tsx
@@ -1,11 +1,18 @@
-import styles from "../styles/introSection.module.css";
+import styles from "../styles/footerSection.module.css";
+import LinkedIn from "../components/icons/linked_icons/LinkedIn";
+import Email from "../components/icons/linked_icons/Email";
+import Github from "../components/icons/linked_icons/Github";
 
 const Footer = () => {
   return (
     <div className={styles.section}>
       <div className={styles.introText}>
-        <h1>Brenden Trieu</h1>
-        <h3>Full Stack Developer</h3>
+        <h2>Made by Brenden Trieu</h2>
+        <div className={styles.iconTray}>
+          <Email />
+          <LinkedIn />
+          <Github />
+        </div>
       </div>
     </div>
   );
